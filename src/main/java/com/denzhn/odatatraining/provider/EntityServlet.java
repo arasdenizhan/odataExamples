9d7cf541361b00496972d22e6d9b7d2517f78c6a
arasdenizhan <denozhan@hotmail.com>
2022-03-29 10:16:16 +0300

    Adds Custom Processor to Custom EdmProvider implementation

--- src/main/java/com/denzhn/odatatraining/provider/EntityServlet.java
+++ src/main/java/com/denzhn/odatatraining/provider/EntityServlet.java
@@ -11,6 +11,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 
 @Slf4j
@@ -19,9 +20,17 @@ public class EntityServlet extends HttpServlet {
     @Override
     protected void service(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException {
         try {
+            HttpSession session = req.getSession(true);
+            DataProvider dataProvider = (DataProvider) session.getAttribute(DataProvider.class.getName());
+            if (dataProvider == null) {
+                dataProvider = new DataProvider();
+                session.setAttribute(DataProvider.class.getName(), dataProvider);
+                log.info("Created new data provider.");
+            }
             OData odata = OData.newInstance();
             ServiceMetadata edm = odata.createServiceMetadata(new EntitiesEdmProvider(), new ArrayList<EdmxReference>());
             ODataHttpHandler handler = odata.createHandler(edm);
+            handler.register(new EntitiesProcessor(dataProvider));
             handler.process(req, resp);
         } catch (RuntimeException e) {
             log.info("server error: " + e);
